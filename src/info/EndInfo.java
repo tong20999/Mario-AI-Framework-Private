1143eb5cf23ebfd129a7d45103e35db76eb461de
Tong Chuiploy <tchuiploy@brandnewday.nl>
2025-06-10 12:32:46 +0700

    update game

--- src/info/EndInfo.java
+++ src/info/EndInfo.java
@@ -5,12 +5,19 @@ import java.text.MessageFormat;
 public class EndInfo {
     public int lose;
     public int win;
-    public int fallKill;
     public int hurt;
     public int timeout;
 
+    public int collect;
+    public long fallKill;
+    public long stompKill;
+    public long shellKill;
+    public long fireKill;
+
     @Override
     public String toString() {
-        return MessageFormat.format("win {0}, lose {1}, timeout {2}, fallKill {3}, hurt {4}", win, lose, timeout, fallKill, hurt);
+        long kill = fallKill + stompKill + shellKill + fireKill;
+        return MessageFormat.format("win {0}, lose {1}, timeout {2}, hurt {3}, collect {4}, kill {5}, {6}, {7}, {8}, {9}",
+                win, lose, timeout, hurt, collect, kill, stompKill, fireKill, shellKill, fallKill);
     }
 }
