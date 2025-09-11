add94f173afb1d686b3bee10bccb40647d005d07
Tong Chuiploy <tchuiploy@brandnewday.nl>
2025-09-11 12:24:04 +0700

    update stack

--- deepRL/multigridstack.py
+++ deepRL/multigridstack.py
@@ -6,50 +6,46 @@ class MultiGridStack(gym.Wrapper):
     def __init__(self, env, num_stack: int):
         super(MultiGridStack, self).__init__(env)
         self.num_stack = num_stack
-        self.keys = list(env.observation_space.spaces.keys())
-        self.frames = {key: deque(maxlen=num_stack) for key in self.keys}
-
+        self.grid_key = 'grid'
+        self.vector_key = 'vector'
+        
+        self.grid_frames = deque(maxlen=num_stack)
         new_obs_spaces = self.env.observation_space.spaces.copy()
         
-        for key in self.keys:
-            space = new_obs_spaces[key]
-            stack_axis = 0 if len(space.shape) > 1 else 0
-            new_shape = list(space.shape)
-            new_shape[stack_axis] *= num_stack
-            
-            new_obs_spaces[key] = gym.spaces.Box(
-                low=np.min(space.low),
-                high=np.max(space.high),
-                shape=tuple(new_shape),
-                dtype=space.dtype
-            )
+        grid_space = new_obs_spaces[self.grid_key]
+
+        new_shape = (num_stack, *grid_space.shape[1:]) # Assumes grid shape is (C, H, W)
+        
+        new_obs_spaces[self.grid_key] = gym.spaces.Box(
+            low=0,
+            high=255, # A safe upper bound for uint8
+            shape=new_shape,
+            dtype=grid_space.dtype
+        )
         
         self.observation_space = gym.spaces.Dict(new_obs_spaces)
-        self.last_obs = None
 
-    def _get_stacked_obs(self):
-        new_obs = self.last_obs.copy()
-        for key in self.keys:
-            axis = 0
-            new_obs[key] = np.concatenate(list(self.frames[key]), axis=axis)
-        return new_obs
+    def _get_stacked_obs(self, last_vector_obs):
+        return {
+            self.grid_key: np.array(self.grid_frames, dtype=self.observation_space[self.grid_key].dtype),
+            self.vector_key: last_vector_obs
+        }
 
     def reset(self, **kwargs):
         obs, info = self.env.reset(**kwargs)
-        self.last_obs = obs
+        grid_obs = obs[self.grid_key]
+        vector_obs = obs[self.vector_key]
         
-        for key in self.keys:
-            self.frames[key].clear()
-            for _ in range(self.num_stack):
-                self.frames[key].append(obs[key])
+        self.grid_frames.clear()
+        for _ in range(self.num_stack):
+            self.grid_frames.append(grid_obs)
             
-        return self._get_stacked_obs(), info
+        return self._get_stacked_obs(vector_obs), info
 
     def step(self, action):
         obs, reward, terminated, truncated, info = self.env.step(action)
-        self.last_obs = obs
+        grid_obs = obs[self.grid_key]
+        vector_obs = obs[self.vector_key]
+        self.grid_frames.append(grid_obs)
         
-        for key in self.keys:
-            self.frames[key].append(obs[key])
-        
-        return self._get_stacked_obs(), reward, terminated, truncated, info
+        return self._get_stacked_obs(vector_obs), reward, terminated, truncated, info
